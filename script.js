3cfa95e98c1bdfc72b1c5add87c280c8d8b7b1b9
yanitejeda <tejedasp@gmail.com>
2021-10-28 13:17:27 +0200

    messaggio stampato a schermo

---
+++ script.js
@@ -0,0 +1,15 @@
+Vue.config.devtools = true;
+
+
+const vueApp = new Vue({
+    el :'#box',
+    data : {
+        messaggio: 'Vue hello',
+    }
+})
+
+
+//Descrizione:
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+//Bonus:
+//Aggiungere alla pagina un’immagine, presa anch’essa da un data.
